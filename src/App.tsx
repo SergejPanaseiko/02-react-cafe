25637cc712b2ccdeebd4cc32c30b1e9968494c22
konsaltpns@gmail.com <konsaltpns@gmail.com>
2026-01-06 10:40:29 +0200

    Add 06012026

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,50 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import css from './App.module.css';
+
 import CafeInfo from './components/CafeInfo/CafeInfo';
 import VoteOptions from './components/VoteOptions/VoteOptions';
 import VoteStats from './components/VoteStats/VoteStats';
 import Notification from './components/Notification/Notification';
+
 import type { Votes, VoteType } from './types/votes';
 
 export default function App() {
-    const [votes, setVotes] = useState<Votes>(() => {
-    const savedVotes = window.localStorage.getItem('saved-votes');
+  const [votes, setVotes] = useState<Votes>(() => {
+    const savedVotes = window.localStorage.getItem('cafe-votes');
     return savedVotes !== null 
       ? JSON.parse(savedVotes) 
       : { good: 0, neutral: 0, bad: 0 };
   });
 
-  useEffect(() => {
-    window.localStorage.setItem('saved-votes', JSON.stringify(votes));
+    useEffect(() => {
+    window.localStorage.setItem('cafe-votes', JSON.stringify(votes));
   }, [votes]);
 
-  const updateVote = (key: VoteType) => {
+  
+  const handleVote = (voteType: VoteType) => {
     setVotes(prev => ({
       ...prev,
-      [key]: prev[key] + 1,
+      [voteType]: prev[voteType] + 1,
     }));
   };
 
-const resetVotes = () => {
+  const handleReset = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
-  const totalVotes = votes.good + votes.neutral + votes.bad;
+   const totalVotes = votes.good + votes.neutral + votes.bad;
   const positiveRate = totalVotes > 0 
     ? Math.round((votes.good / totalVotes) * 100) 
     : 0;
 
   return (
-    <div>
+    <div className={css.container}>
       <CafeInfo />
-      
       <VoteOptions
-        onVote={updateVote}
-        onReset={resetVotes}
-        canReset={totalVotes > 0}
+        onVote={handleVote}
+        onReset={handleReset}
+        canReset={totalVotes > 0} 
       />
-
       {totalVotes > 0 ? (
         <VoteStats 
           votes={votes} 
